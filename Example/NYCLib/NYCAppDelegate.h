a8660f050bbc040cd2f06072d3da651b001924a6
牛永昌 <yongchang.niu@dnurse.cn>
2021-04-07 13:04:02 +0800

    Initial commit

---
+++ Example/NYCLib/NYCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  NYCAppDelegate.h
+//  NYCLib
+//
+//  Created by 牛永昌 on 04/07/2021.
+//  Copyright (c) 2021 牛永昌. All rights reserved.
+//
+
+@import UIKit;
+
+@interface NYCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
