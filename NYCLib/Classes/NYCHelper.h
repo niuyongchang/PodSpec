eb3a13172327b6e1eb55a7ef2215b6588d67073e
牛永昌 <yongchang.niu@dnurse.cn>
2021-04-07 13:51:50 +0800

    modify

---
+++ NYCLib/Classes/NYCHelper.h
@@ -0,0 +1,19 @@
+//
+//  NYCHelper.h
+//  NYCLib_Example
+//
+//  Created by 牛永昌 on 2021/4/7.
+//  Copyright © 2021 牛永昌. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface NYCHelper : NSObject
+
+- (int)date;
+
+@end
+
+NS_ASSUME_NONNULL_END
